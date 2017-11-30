bfec46fd8f3c25a5be80f087cfd3493984c8c39e
Rafael PacMe <rafy.pachecomendez@gmail.com>
2017-11-30 14:52:54 -0300

    goodbye more things of Motorola

--- msm8610.mk
+++ msm8610.mk
@@ -12,12 +12,12 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-COMMON_PATH := device/motorola/msm8610-common
+COMMON_PATH := device/microsoft/msm8610-common
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
 # common vendor blobs
-$(call inherit-product, vendor/motorola/msm8610-common/msm8610-common-vendor.mk)
+$(call inherit-product, vendor/microsoft/msm8610-common/msm8610-common-vendor.mk)
 
 # Ramdisk
 PRODUCT_PACKAGES += \
